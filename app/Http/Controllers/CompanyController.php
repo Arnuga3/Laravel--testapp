091d0fb36cafc2034b5cf261c673635e050592fb
Arnis Zelcs <arniszelcs@Arniss-MacBook-Pro.local>
2018-10-12 07:59:34 +0100

    Add routes authentication middleware to controllers

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -9,6 +9,11 @@ use Illuminate\Http\File;
 
 class CompanyController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
     /**
      * Display a listing of the resource.
      *
